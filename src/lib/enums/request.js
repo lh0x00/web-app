cb5362e3e1793bca36813b069caa1729c9030d2b
Lam Hieu <lamhieu.vk@gmail.com>
2018-09-19 21:49:49 +0700

    add register page and register form, handle create user by api

--- src/lib/enums/request.js
+++ src/lib/enums/request.js
@@ -3,8 +3,9 @@
 export default {
   BASE_URL: 'http://localhost:4000/api',
   METHOD: 'post',
-  TIMEOUT: 15000,
+  TIMEOUT: 5000,
   API: {
     LOGIN: '/account/login',
+    REGISTER: '/account/register',
   },
 }
