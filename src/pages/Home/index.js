c49e6291ffd305826f12e7968c6cafba3b51407a
Lam Hieu <lamhieu.vk@gmail.com>
2018-09-21 22:25:27 +0700

    update display text message in home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,15 +1,24 @@
 import React, { PureComponent } from 'react'
 import compose from 'lib/hoc/compose'
 import withSession from 'lib/hoc/withSession'
+import withRouter from 'lib/hoc/withRouter'
 
-class Home extends PureComponent<PropsComponent, StateComponent> {
+type THome = {} & Page
+
+class Home extends PureComponent<THome, StateComponent> {
   render() {
+    const { session } = this.props
+    const { isLogged } = session || {}
     return (
       <div>
         <div>welcome to home!</div>
+        <div>{isLogged ? 'logged' : 'not logged'}</div>
       </div>
     )
   }
 }
 
-export default compose(withSession)(Home)
+export default compose(
+  withSession,
+  withRouter,
+)(Home)
